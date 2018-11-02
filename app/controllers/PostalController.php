97a2beec10e15bbc37b50dc5ffdc842f09f6dc7f
Luca Oonk <luca.oonk@gmail.com>
2018-11-02 11:23:01 +0100

    Added postal check controller met werkende API connectie

--- app/controllers/PostalController.php
+++ app/controllers/PostalController.php
@@ -11,25 +11,33 @@ class PostalControllerontroller
 
  function PostalCheck($code, $num){
 
-$client = new http\Client;
-$request = new http\Client\Request;
+     $curl = curl_init();
 
-$request->setRequestUrl('https://api.postcodeapi.nu/v2/addresses/');
-$request->setRequestMethod('GET');
-$request->setQuery(new http\QueryString(array(
-    'postcode' => $code,
-    'number' => $num
-)));
+     curl_setopt_array($curl, array(
+         CURLOPT_URL => "https://api.postcodeapi.nu/v2/addresses/?postcode=$code&number=$num",
+         CURLOPT_RETURNTRANSFER => true,
+         CURLOPT_ENCODING => "",
+         CURLOPT_MAXREDIRS => 10,
+         CURLOPT_TIMEOUT => 30,
+         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+         CURLOPT_CUSTOMREQUEST => "GET",
+         CURLOPT_HTTPHEADER => array(
+             "accept: application/hal+json",
+             "x-api-key: 70krf7hu8X2mR77IaFZBl6y1VvRoHl5G8BID0eHG"
+         ),
+     ));
 
-$request->setHeaders(array(
-    'accept' => 'application/hal+json',
-    'x-api-key' => 'a0B1c2D34D5c6b7a8'
-));
+     $response = curl_exec($curl);
+     $err = curl_error($curl);
 
-$client->enqueue($request)->send();
-$response = $client->getResponse();
+     curl_close($curl);
 
-echo $response->getBody();
+     if ($err) {
+         return ("cURL Error #:" . $err);
+     } else {
+         var_dump(json_decode($response)->_embedded->addresses[0]);
+
+     }
  }
 
 
