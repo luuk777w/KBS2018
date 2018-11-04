7ff28c66384bb337fedcbd7d58e79a340b706110
JustaRico <44608076+JustaRico@users.noreply.github.com>
2018-11-04 16:46:23 +0100

    I don't want to commit
    I don't want to commit to master

---
+++ cache/ba7fc337b36ee63540a67b887b692141c5fc2a02.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Wide World Importers</title>
+    <style>
+        .footer_background {
+        "background-color: yellow"
+
+        }
+    </style>
+
+    <?php echo $__env->yieldContent('head'); ?>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
+    <script defer src="https://use.fontawesome.com/releases/v5.4.2/js/all.js" integrity="sha384-wp96dIgDl5BLlOXb4VMinXPNiB32VYBSoXOoiARzSTXY+tsK8yDTYfvdTyqzdGGN" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+<div class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Wide World Importers</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="kbs2018/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kbs2018/products">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <?php 
+                        $aantalitems = 0;
+                        $cookie_data = stripslashes($_COOKIE['shopping_cart']);
+                        $cart_data = json_decode($cookie_data, true);
+                        foreach ($cart_data as $key => $value){
+                            $aantalitems++;
+                        }
+                        if($aantalitems == 0){
+                            $aantalitems = 0;
+                        }
+                        print('<a href="kbs2018/shoppingcart"><button type="button" class="btn btn-primary">
+Winkelwagen <span class="badge badge-light">'.$aantalitems.'</span>
+</button></a>');
+                     ?>
+                </li>
+                <li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="kbs2018/register">Registreer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kbs2018/login">Login</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <?php echo $__env->yieldContent('body'); ?>
+
+
+
+    <footer  class="page-footer font-small blue pt-4">
+
+        <!-- Footer Links -->
+        <div  class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row ">
+
+                <!-- Grid column -->
+                <div  class="col-md-6 mt-md-0 mt-3 bg-light">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase">Contactgegevens</h5>
+                    <p>Email: <br>
+                    Telefoon: <br>
+                    Adres: </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3 bg-light">
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3 bg-light">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 4</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3 bg-light">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 4</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3 bg-light">© 2018 Copyright:
+            <a style="text-decoration: none; color: black" href="https://youtu.be/9Mq6u6zc7qU?t=18"> WWI</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+</div>
+
+
+
+<?php echo $__env->yieldContent('scripts'); ?>
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
