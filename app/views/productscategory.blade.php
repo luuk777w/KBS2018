440e87e8a0533a944e87818b37b42e982d6f8f94
Luuk <luuk.wuijster@hotmail.com>
2018-11-06 17:13:13 +0100

    HONDERSTE COMMIT!!!! WOHOOOOOOO!!!

--- app/views/productscategory.blade.php
+++ app/views/productscategory.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Wide World Importers GANG</title>
 
-
     <style>
         
         /* In de Head section kan je allemaal tags plaatsen die daar moeten zoals een style tag, 
@@ -20,8 +19,6 @@
 
         <div style="margin: auto; width: 58rem; overflow: auto;">
     @foreach ($products as $product)
-
-
 
             <div class="card" style="width: 18rem; height:40rem;float:left;margin:10px">
                 <img class="card-img-top" src="https://res.cloudinary.com/teepublic/image/private/s--tM4JElmV--/t_Preview/b_rgb:191919,c_limit,f_auto,h_313,q_90,w_313/v1491250418/production/designs/1381795_1" class="img-thumbnail" alt="Card image cap">
