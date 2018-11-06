f307cb0587918f1be02560306313233eaeb85e54
JustaRico <ricodegrijs@gmail.com>
2018-11-06 13:11:21 +0100

    Revert "Merge branch 'master' of https://github.com/luuk777w/KBS2018"
    This reverts commit f3e1b783fe03e8d47fdf739160adfd69c2c2ec5d, reversing
    changes made to 3644cf5981b141901354940fd15a025828c76d7a.

--- app/models/Products.php
+++ app/models/Products.php
@@ -8,11 +8,7 @@ class Products extends Model
 {
     public function getProducts()
     {
-<<<<<<< HEAD
-        $data = $this->db->sql("select * from stockitems Join stockitems_media using(stockitemID) where stockitems_media.Primary=1");
-=======
         $data = $this->db->sql("SELECT * FROM stockitems SI JOIN stockitemstockgroups SG on SI.StockItemID=SG.StockitemID JOIN stockgroups sgg on SG.stockgroupid=sgg.stockgroupid ");
->>>>>>> parent of 1721fa4... Merge branch 'master' of https://github.com/luuk777w/KBS2018
 
         return $data;
     }
