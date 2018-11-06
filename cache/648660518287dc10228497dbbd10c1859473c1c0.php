a46797d5e7d0aac15beb88c5503f3c2c8aa3de87
Matthijsn <Matthijsn@users.noreply.github.com>
2018-11-06 13:50:17 +0100

    ..

--- cache/648660518287dc10228497dbbd10c1859473c1c0.php
+++ cache/648660518287dc10228497dbbd10c1859473c1c0.php
@@ -1,25 +1,7 @@
 <?php $__env->startSection('body'); ?>
 
     <br>
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	<h1>Deploy Test 4</h1>
-=======
-	<h1>Deploy Test 4</h1>
-=======
-	<h1>Deploy Test</h1>
->>>>>>> parent of 11b64ce... Deploy Test
->>>>>>> af732c0b52abeed04d537fcb19131b206bb02a31
-=======
-	<h1>Deploy Test 4</h1>
 
-
->>>>>>> 1721fa4df2c2363beee30f7bd6bf791d719ddc13
-
->>>>>>> 4730f054e104e39820fadd0dc45170acbc469edd
     <div class="jumbotron">
         <h1 class="display-3">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
