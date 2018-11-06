084982b5511b5548fd05182ef911da4ebf61d60f
JustaRico <ricodegrijs@gmail.com>
2018-11-06 13:11:09 +0100

    Revert "Update app/views/home.blade.php"
    This reverts commit db066e8c3aebd68ec86db75b0f95895b57835844.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('body')
 
-<<<<<<< HEAD
 <br>
 
 <div class="jumbotron">
@@ -14,40 +13,5 @@
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </p>
 </div>
-=======
-    <br>
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	<h1>Deploy Test 4</h1>
-=======
-	<h1>Deploy Test 4</h1>
-=======
-	<h1>Deploy Test</h1>
->>>>>>> parent of 11b64ce... Deploy Test
->>>>>>> af732c0b52abeed04d537fcb19131b206bb02a31
-
->>>>>>> 4730f054e104e39820fadd0dc45170acbc469edd
-=======
->>>>>>> 1721fa4df2c2363beee30f7bd6bf791d719ddc13
-=======
-	<h1>Deploy Test 4</h1>
-
-
-
->>>>>>> parent of 1721fa4... Merge branch 'master' of https://github.com/luuk777w/KBS2018
-    <div class="jumbotron">
-        <h1 class="display-3">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </p>
-    </div>
->>>>>>> a902a822676693530c8826bfc5e3f6b802312310
 
 @endsection
