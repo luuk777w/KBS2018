b7be0d43d25598648d283fbcaf3d38cc54a2ef36
JustaRico <ricodegrijs@gmail.com>
2018-11-13 12:42:09 +0100

    ?

--- cache/c78435dced4b27ccabca3e24a257bf8ee6c9ff54.php
+++ cache/c78435dced4b27ccabca3e24a257bf8ee6c9ff54.php
@@ -107,12 +107,13 @@
         <img src="/assets/img/usb_missle_launcher_gray_1.jpg">
 
         <br>
-        pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
-        vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-        Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.
-        Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.
-        Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+        Een geweldig wapen dat je kan gebruiken om
+        <br>
+        je ergste vijanden mee te vernietigen
+        <br>
+        (En misschien ook L<b>ou</b>ca)
+        <br>
+
     </div>
 
     <?php $__env->stopSection(); ?>
