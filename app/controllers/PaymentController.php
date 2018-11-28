c8601bfe626d851e5ed6f3c177c4c62b0fc8664b
Luuk Wuijster <luuk.wuijster@hotmail.com>
2018-11-28 12:02:31 +0100

    MOL IN JE HOL

--- app/controllers/PaymentController.php
+++ app/controllers/PaymentController.php
@@ -2,45 +2,40 @@
 
 namespace App\Controllers;
 
-use Core\Controller;
-use Core\Auth;
-use App\Models\Categories;
-use App\Models\Products;
-use Mollie\Api\MollieApiClient;
 use App\Models\Orders;
+use Core\Auth;
+use Core\Controller;
+use Mollie\Api\MollieApiClient;
 
 class PaymentController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
 
         session_start();
 
-        if(!isset($_SESSION["orderTotal"])) {
+        if (!isset($_SESSION["orderTotal"])) {
             return "Winkelwagen leeg";
         }
 
         $mollie = new MollieApiClient();
         $mollie->setApiKey("test_RAnWWDeHWDMPhCzeEMvq9FtNyfgwDD");
 
-
         $formattedTotal = number_format(round($_SESSION["orderTotal"], 2), 2);
 
         $payment = $mollie->payments->create([
             "amount" => [
                 "currency" => "EUR",
-                "value" => $formattedTotal
+                "value" => $formattedTotal,
             ],
             "description" => "Bestelling bij de WWI",
             "redirectUrl" => "https://wide-world-importers.cf/bedankt",
-            "webhookUrl"  => "https://wide-world-importers.cf/mollie-webhook/",
+            "webhookUrl" => "https://wide-world-importers.cf/mollie-webhook/",
         ]);
 
-        $order = new Orders();
-        $order->test();
-
         $auth = new Auth();
-        $auth->setOneTimeAuthorization("bedankPage"); 
+        $auth->setOneTimeAuthorization("bedankPage");
         setcookie("shopping_cart", "", time() - 3600, '/');
 
         return header("Location: " . $payment->getCheckoutUrl(), true, 303);
@@ -57,39 +52,41 @@ class PaymentController extends Controller
 
             if ($payment->isPaid() && !$payment->hasRefunds() && !$payment->hasChargebacks()) {
 
+                return header("Location: /products");
+
                 $order = new Orders();
                 $order->test();
 
             } elseif ($payment->isOpen()) {
                 /*
-                 * The payment is open.
-                 */
+             * The payment is open.
+             */
             } elseif ($payment->isPending()) {
                 /*
-                 * The payment is pending.
-                 */
+             * The payment is pending.
+             */
             } elseif ($payment->isFailed()) {
                 /*
-                 * The payment has failed.
-                 */
+             * The payment has failed.
+             */
             } elseif ($payment->isExpired()) {
                 /*
-                 * The payment is expired.
-                 */
+             * The payment is expired.
+             */
             } elseif ($payment->isCanceled()) {
                 /*
-                 * The payment has been canceled.
-                 */
+             * The payment has been canceled.
+             */
             } elseif ($payment->hasRefunds()) {
                 /*
-                 * The payment has been (partially) refunded.
-                 * The status of the payment is still "paid"
-                 */
+             * The payment has been (partially) refunded.
+             * The status of the payment is still "paid"
+             */
             } elseif ($payment->hasChargebacks()) {
                 /*
-                 * The payment has been (partially) charged back.
-                 * The status of the payment is still "paid"
-                 */
+             * The payment has been (partially) charged back.
+             * The status of the payment is still "paid"
+             */
             }
 
         } catch (\Mollie\Api\Exceptions\ApiException $e) {
@@ -97,12 +94,11 @@ class PaymentController extends Controller
         }
     }
 
-    public function bedankPage() 
+    public function bedankPage()
     {
         $auth = new Auth();
 
-        if(!$auth->isAuthorized()) 
-        {
+        if (!$auth->isAuthorized()) {
             return $auth->error401();
         }
 
